5cc776381572d5048a1aada8421c1df898f3c555
DieWachtel <olivia.wachtel@googlemail.com>
2020-06-01 14:08:56 -0700

    lul

---
+++ lib/models/user.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+User userFromJson(String str) => User.fromJson(json.decode(str));
+
+String userToJson(User data) => json.encode(data.toJson());
+
+class User {
+  String name;
+  String age;
+  String email;
+  String password;
+
+  User({
+    this.name,
+    this.age,
+    this.email,
+    this.password,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+    name: json["name"],
+    age: json["age"],
+    email: json["email"],
+    password: json["password"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "name": name,
+    "age": age,
+    "email": email,
+    "password": password,
+  };
+}
