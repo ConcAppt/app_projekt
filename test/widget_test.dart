a88ec0f3bf20e9bb826303bcdf46f1a3d5a3ade2
paxbo <paul.borutta@st.ovgu.de>
2020-08-12 12:21:49 +0200

    aber jetzt

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import '../lib/utils/main.dart';
-//import 'file:///C:/Users/olivi/StudioProjects/app_projekt/lib/utils/main.dart';
+//mport 'file:///C:/Users/olivi/StudioProjects/app_projekt/lib/utils/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
